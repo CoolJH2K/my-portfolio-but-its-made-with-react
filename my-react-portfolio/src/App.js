c5bb78c92dd2207259b6e4fa0ef0cbe3a2c3d7b4
Janna Harmon <harmonjanna00@gmail.com>
2023-06-05 21:09:12 -0400

    Added css files for components.

--- my-react-portfolio/src/App.js
+++ my-react-portfolio/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // Import components
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Homepage from "./components/Homepage";
 import Navigation from "./components/Navigation/Navigation";
 import Project from "./components/Project/Project";
 
